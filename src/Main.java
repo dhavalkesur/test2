f2f513256c1941a98425c1ac5791209259f300eb
= <=>
2018-12-01 15:44:14 +0530

    First Version

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import org.jetbrains.annotations.Contract;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        int j = 0;
+        for (int i = 0; i <= 10; i++)
+        {
+            j = i;
+        }
+        System.out.println(j);
+        int test = Main.outputData();
+    }
+
+    public static Integer outputData()
+    {
+        return 0;
+    }
+}
+
